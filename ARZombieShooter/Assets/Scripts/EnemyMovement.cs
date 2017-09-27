2642c61a95068cae0ebbfaa3251ba19571a2111d
Boris Filipovic <boris.filipovic1@gmail.com>
2017-09-27 15:11:26 +0200

    Added enemy movement animation. Created enemy movement script so enemy will always walk towards player.

---
+++ ARZombieShooter/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+	public float movementSpeed = 0.3f;
+
+	// Update is called once per frame
+	void Update () {
+		/// Move enemy forward.
+		transform.Translate(Vector3.forward * Time.deltaTime * movementSpeed);
+
+		/// Move enemy in direction of the player (main camera).
+		transform.LookAt(Camera.main.transform.position);
+
+		/// Make sure that enemy is only moving on x component.
+		transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+	}
+}
