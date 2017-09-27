ccf0d7e07a12e90e6f8ab7319d690ddfcda7f44e
Boris Filipovic <boris.filipovic1@gmail.com>
2017-09-27 15:43:34 +0200

    Added enemy attack animation and script.

---
+++ ARZombieShooter/Assets/Scripts/CollisionWithPlayer.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionWithPlayer : MonoBehaviour {
+
+	// Public properties.
+	public int timeBetweenAttack = 2;
+
+	// Private properties.
+	private string playerTag = "MainCamera";
+	private bool zombieCollided = false;
+	private float timer;
+
+	// Use this for initialization
+	void Start () {
+		/// Set initial values.
+		timer = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/// Set timer on every frame.
+		timer += Time.deltaTime;
+
+		if (zombieCollided && timer >= timeBetweenAttack ) {
+			/// Zombie have collided with player so start attack animation.
+			Attack ();
+		}
+	}
+
+	// Collision detected.
+	void OnCollisionEnter (Collision collision) {
+		if (collision.gameObject.tag == playerTag) {
+			Debug.Log ("Enter collision");
+			zombieCollided = true;
+		}
+	}
+
+	// Collision ended.
+	void OnCollisionExit (Collision collision) {
+		if (collision.gameObject.tag == playerTag) {
+			Debug.Log ("Enter collision");
+			zombieCollided = false;
+		}
+	}
+
+	// Attack.
+	void Attack () {
+		/// Reset timer every time enemy attacks player.
+		timer = 0.0f;
+
+		/// Play attack animation.
+		GetComponent<Animator> ().Play("attack");
+	}
+}
